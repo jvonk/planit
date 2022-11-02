557a38ff936520bee2516e17344c5777e0b4cb85
planit <planit@tanjeffreyz.github.io>
2022-11-02 09:34:01 +0000

    Updated assignments

---
+++ data/assignments.js
@@ -0,0 +1,306 @@
+assignments = {
+  "CS 170": [
+    {
+      "title": "Homework 01",
+      "course": "CS 170",
+      "dueDate": "2022-09-05T21:59:00",
+      "link": "https://www.gradescope.com/courses/426018/assignments/2215801/submissions/132071334",
+      "submitted": true
+    },
+    {
+      "title": "Homework 02",
+      "course": "CS 170",
+      "dueDate": "2022-09-12T21:59:00",
+      "link": "https://www.gradescope.com/courses/426018/assignments/2226056/submissions/133353395",
+      "submitted": true
+    },
+    {
+      "title": "Homework 03",
+      "course": "CS 170",
+      "dueDate": "2022-09-19T21:59:00",
+      "link": "https://www.gradescope.com/courses/426018/assignments/2226061/submissions/134916095",
+      "submitted": true
+    },
+    {
+      "title": "Homework 04",
+      "course": "CS 170",
+      "dueDate": "2022-09-26T21:59:00",
+      "link": "https://www.gradescope.com/courses/426018/assignments/2226062/submissions/136527934",
+      "submitted": true
+    },
+    {
+      "title": "Homework 05",
+      "course": "CS 170",
+      "dueDate": "2022-10-03T21:59:00",
+      "link": "https://www.gradescope.com/courses/426018",
+      "submitted": false
+    },
+    {
+      "title": "Homework 06",
+      "course": "CS 170",
+      "dueDate": "2022-10-11T21:59:00",
+      "link": "https://www.gradescope.com/courses/426018/assignments/2226064/submissions/139894445",
+      "submitted": true
+    },
+    {
+      "title": "Homework 07",
+      "course": "CS 170",
+      "dueDate": "2022-10-17T21:59:00",
+      "link": "https://www.gradescope.com/courses/426018/assignments/2226066/submissions/141717763",
+      "submitted": true
+    },
+    {
+      "title": "Homework 08",
+      "course": "CS 170",
+      "dueDate": "2022-10-24T21:59:00",
+      "link": "https://www.gradescope.com/courses/426018/assignments/2226067/submissions/143498819",
+      "submitted": true
+    },
+    {
+      "title": "Homework 09",
+      "course": "CS 170",
+      "dueDate": "2022-10-31T21:59:00",
+      "link": "https://www.gradescope.com/courses/426018",
+      "submitted": false
+    },
+    {
+      "title": "Homework 10",
+      "course": "CS 170",
+      "dueDate": "2022-11-07T21:59:00",
+      "link": "https://www.gradescope.com/courses/426018",
+      "submitted": false
+    },
+    {
+      "title": "Mid Semester Feedback",
+      "course": "CS 170",
+      "dueDate": "2022-11-01T00:10:00",
+      "link": "https://www.gradescope.com/courses/426018",
+      "submitted": false
+    }
+  ],
+  "Math 110": [
+    {
+      "title": "Practice with Gradescope - Optional",
+      "course": "Math 110",
+      "dueDate": "2022-09-26T22:00:00",
+      "link": "https://www.gradescope.com/courses/423294",
+      "submitted": false
+    },
+    {
+      "title": "Midterm 1",
+      "course": "Math 110",
+      "dueDate": "2022-09-27T23:00:00",
+      "link": "https://www.gradescope.com/courses/423294/assignments/2293074/submissions/136760810",
+      "submitted": true
+    }
+  ],
+  "Math 110 - DIS 101, 103, 104": [
+    {
+      "title": "Homework 1",
+      "course": "Math 110 - DIS 101, 103, 104",
+      "dueDate": "2022-09-02T23:59:00",
+      "link": "https://www.gradescope.com/courses/431399/assignments/2210263/submissions/131853976",
+      "submitted": true
+    },
+    {
+      "title": "Homework 2",
+      "course": "Math 110 - DIS 101, 103, 104",
+      "dueDate": "2022-09-09T23:59:00",
+      "link": "https://www.gradescope.com/courses/431399/assignments/2210265/submissions/132874071",
+      "submitted": true
+    },
+    {
+      "title": "Homework 3",
+      "course": "Math 110 - DIS 101, 103, 104",
+      "dueDate": "2022-09-16T23:59:00",
+      "link": "https://www.gradescope.com/courses/431399/assignments/2210268/submissions/134396853",
+      "submitted": true
+    },
+    {
+      "title": "Homework 4",
+      "course": "Math 110 - DIS 101, 103, 104",
+      "dueDate": "2022-09-23T23:59:00",
+      "link": "https://www.gradescope.com/courses/431399/assignments/2210282/submissions/136125870",
+      "submitted": true
+    },
+    {
+      "title": "Homework 5",
+      "course": "Math 110 - DIS 101, 103, 104",
+      "dueDate": "2022-10-03T23:59:00",
+      "link": "https://www.gradescope.com/courses/431399/assignments/2285477/submissions/137974297",
+      "submitted": true
+    },
+    {
+      "title": "Homework 6",
+      "course": "Math 110 - DIS 101, 103, 104",
+      "dueDate": "2022-10-07T23:59:00",
+      "link": "https://www.gradescope.com/courses/431399/assignments/2285479/submissions/139035102",
+      "submitted": true
+    },
+    {
+      "title": "Homework 8",
+      "course": "Math 110 - DIS 101, 103, 104",
+      "dueDate": "2022-10-21T23:59:00",
+      "link": "https://www.gradescope.com/courses/431399/assignments/2307895/submissions/142327556",
+      "submitted": true
+    },
+    {
+      "title": "Homework 7 ",
+      "course": "Math 110 - DIS 101, 103, 104",
+      "dueDate": "2022-10-14T23:59:00",
+      "link": "https://www.gradescope.com/courses/431399/assignments/2322499/submissions/142326186",
+      "submitted": true
+    },
+    {
+      "title": "Homework 9",
+      "course": "Math 110 - DIS 101, 103, 104",
+      "dueDate": "2022-10-28T23:59:00",
+      "link": "https://www.gradescope.com/courses/431399/assignments/2347332/submissions/144912006",
+      "submitted": true
+    },
+    {
+      "title": "Homework 10",
+      "course": "Math 110 - DIS 101, 103, 104",
+      "dueDate": "2022-11-04T23:59:00",
+      "link": "https://www.gradescope.com/courses/431399",
+      "submitted": false
+    }
+  ],
+  "PHYSICS 5CL-LAB": [
+    {
+      "title": "Pre-lab 0",
+      "course": "PHYSICS 5CL-LAB",
+      "dueDate": "2022-08-31T14:00:00",
+      "link": "https://www.gradescope.com/courses/427240/assignments/2190833/submissions/131204022",
+      "submitted": true
+    },
+    {
+      "title": "Pre-lab 1",
+      "course": "PHYSICS 5CL-LAB",
+      "dueDate": "2022-09-14T14:00:00",
+      "link": "https://www.gradescope.com/courses/427240/assignments/2211614/submissions/133615458",
+      "submitted": true
+    },
+    {
+      "title": "Pre-lab 2",
+      "course": "PHYSICS 5CL-LAB",
+      "dueDate": "2022-09-26T14:00:00",
+      "link": "https://www.gradescope.com/courses/427240/assignments/2212646/submissions/136238328",
+      "submitted": true
+    },
+    {
+      "title": "Pre-Lab 3",
+      "course": "PHYSICS 5CL-LAB",
+      "dueDate": "2022-10-03T14:00:00",
+      "link": "https://www.gradescope.com/courses/427240/assignments/2212690/submissions/137994953",
+      "submitted": true
+    },
+    {
+      "title": "Pre-Lab 4",
+      "course": "PHYSICS 5CL-LAB",
+      "dueDate": "2022-10-10T21:00:00",
+      "link": "https://www.gradescope.com/courses/427240/assignments/2212703/submissions/139716584",
+      "submitted": true
+    },
+    {
+      "title": "Pre-Lab 6",
+      "course": "PHYSICS 5CL-LAB",
+      "dueDate": "2022-10-27T14:00:00",
+      "link": "https://www.gradescope.com/courses/427240/assignments/2212710/submissions/143433879",
+      "submitted": true
+    },
+    {
+      "title": "Lab 0 Submission",
+      "course": "PHYSICS 5CL-LAB",
+      "dueDate": "2022-09-21T23:59:00",
+      "link": "https://www.gradescope.com/courses/427240/assignments/2250764/submissions/134825671",
+      "submitted": true
+    },
+    {
+      "title": "Lab 1 Submission",
+      "course": "PHYSICS 5CL-LAB",
+      "dueDate": "2022-09-28T23:59:00",
+      "link": "https://www.gradescope.com/courses/427240/assignments/2301158/submissions/137124939",
+      "submitted": true
+    },
+    {
+      "title": "Lab 2 Submission",
+      "course": "PHYSICS 5CL-LAB",
+      "dueDate": "2022-10-05T23:59:00",
+      "link": "https://www.gradescope.com/courses/427240/assignments/2325014/submissions/138861636",
+      "submitted": true
+    },
+    {
+      "title": "Lab 3 Submission",
+      "course": "PHYSICS 5CL-LAB",
+      "dueDate": "2022-10-14T23:59:00",
+      "link": "https://www.gradescope.com/courses/427240/assignments/2325727/submissions/141169806",
+      "submitted": true
+    },
+    {
+      "title": "Lab 4 Submission",
+      "course": "PHYSICS 5CL-LAB",
+      "dueDate": "2022-10-19T23:59:00",
+      "link": "https://www.gradescope.com/courses/427240/assignments/2362272/submissions/142317181",
+      "submitted": true
+    },
+    {
+      "title": "Lab 5 Submission",
+      "course": "PHYSICS 5CL-LAB",
+      "dueDate": "2022-10-26T23:59:00",
+      "link": "https://www.gradescope.com/courses/427240",
+      "submitted": false
+    }
+  ],
+  "Physics 5C": [
+    {
+      "title": "Homework 1",
+      "course": "Physics 5C",
+      "dueDate": "2022-09-09T23:00:00",
+      "link": "https://www.gradescope.com/courses/434251/assignments/2224434/submissions/132797116",
+      "submitted": true
+    },
+    {
+      "title": "Homework 2",
+      "course": "Physics 5C",
+      "dueDate": "2022-09-16T23:59:00",
+      "link": "https://www.gradescope.com/courses/434251/assignments/2241832/submissions/134396698",
+      "submitted": true
+    },
+    {
+      "title": "Homework 3",
+      "course": "Physics 5C",
+      "dueDate": "2022-09-23T23:59:00",
+      "link": "https://www.gradescope.com/courses/434251/assignments/2262296/submissions/135975934",
+      "submitted": true
+    },
+    {
+      "title": "Homework 4",
+      "course": "Physics 5C",
+      "dueDate": "2022-09-30T23:59:00",
+      "link": "https://www.gradescope.com/courses/434251/assignments/2285987/submissions/137647986",
+      "submitted": true
+    },
+    {
+      "title": "Homework 5",
+      "course": "Physics 5C",
+      "dueDate": "2022-10-07T23:59:00",
+      "link": "https://www.gradescope.com/courses/434251",
+      "submitted": false
+    },
+    {
+      "title": "Homework 6",
+      "course": "Physics 5C",
+      "dueDate": "2022-10-31T23:59:00",
+      "link": "https://www.gradescope.com/courses/434251/assignments/2378664/submissions/145173702",
+      "submitted": true
+    },
+    {
+      "title": "Homework 7",
+      "course": "Physics 5C",
+      "dueDate": "2022-11-07T23:59:00",
+      "link": "https://www.gradescope.com/courses/434251",
+      "submitted": false
+    }
+  ]
+};
