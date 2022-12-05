a745e821ed564bd4d4e18de96960a51e2b2cd0d0
planit <planit@tanjeffreyz.github.io>
2022-12-05 02:54:25 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -130,8 +130,8 @@ assignments = {
       "title": "Project Code",
       "course": "CS 170",
       "dueDate": "2022-12-04T23:59:00",
-      "link": "https://www.gradescope.com/courses/426018",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/426018/assignments/2477803/submissions/152421382",
+      "submitted": true
     }
   ],
   "Math 110": [
